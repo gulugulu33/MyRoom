20a1737d3b44c61db22556f1ef69afe1ca78d820
Kijasss <w95722807@163.com>
2022-05-21 15:45:00 +0800

    first commit

---
+++ package/client/pages/drawPanel/index.tsx
@@ -0,0 +1,10 @@
+import React, { memo } from 'react'
+import './style.css'
+
+export default memo(function drawPanel() {
+  return (
+    <div className='draw-panel'>
+      画布区
+    </div>
+  )
+})
---
+++ package/client/pages/leftPanel/index.tsx
@@ -0,0 +1,9 @@
+import React, { memo } from 'react'
+
+const leftPanel = memo(() => {
+  return (
+    <div className='left-panel'>组件区</div>
+  )
+})
+
+export default leftPanel
